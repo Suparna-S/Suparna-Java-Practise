a28e676ad4a93a7e356478b53f7c11dbbe37c604
Suparna S <ssuparna98@gmail.com>
2020-02-12 10:30:00 +0530

    Update SBuffer.java

--- SBuffer.java
+++ SBuffer.java
@@ -1,3 +1,14 @@
+/*Write a program which creates a StringBuffer “This is StringBuffer” and
+ performs the following. 
+
+1.	Adds the string ”- This is a sample program” to existing string and 
+	display it.
+2.	Inserts the string “Object” into the existing string at 21st postion
+	 and display it.
+3.	Reverses the entire string and displays it.
+4.	Replaces the word “Buffer” with “Builder” and display it.
+*/
+
 import java.lang.String;
 class SBuffer
 {
