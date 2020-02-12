c7014e2dbf169929510601cfcc345f9e5ef9bf14
Suparna S <ssuparna98@gmail.com>
2020-02-12 10:41:21 +0530

    Update Folder.java

--- Folder.java
+++ Folder.java
@@ -1,3 +1,15 @@
+/* Write a program which creates a String “C:\IBM\DB2\PROGRAM\DB2COPY1.EXE”. 
+It parses the string with the delimiter as ‘\’ and displays the String in the
+ following format.
+Drive: c:\
+Folders: IBM || DB2 || PROGRAM 
+File: DB2COPY1.EXE
+
+Hint:  Use String Builder for concatenating the folder names with |
+*/
+
+
+
 import java.lang.*;
 import java.util.*;
 
